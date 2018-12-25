fee4bf645f22d72d23d54ecce405b73b3410f051
cheng <1504468447@qq.com>
2018-12-25 14:35:56 +0800

    添加授权登录请求

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 //app.js
 App({
   onLaunch: function () {
+    let that = this
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.getSystemInfo({
+      success(res) {
+        if (res.platform === 'devtools') {
+          /* 开发环境 */
+          that.globalData.BASE_URL = 'http://localhost:8804/apis/api'
+        } else {
+          /* 线上 */
+          that.globalData.BASE_URL = 'https://dev.lionynn.cn/apis/api'
+        }
+      }
+    })
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         var code = res.code; //返回code
-        console.log(res.code);
         this.globalData.js_code = res.code
       }
     })
   },
   globalData: {
-    // BASE_URL: 'https://dev.lionynn.cn/apis/api',
-    BASE_URL: 'http://localhost:8804/apis/api',
+    BASE_URL: '',
     userInfo: null,
     js_code: null
   }
--- pages/getScene/getScene.js
+++ pages/getScene/getScene.js
@@ -4,44 +4,136 @@ const app = getApp()
 
 Page({
   data: {
-    scene: '',
+    resultText: '',
+    scene: 1,
+    canIUse: true,
+    showModalStatus: false,
+    systemUserInfo: null
   },
   onLoad(query) {
     var scene = decodeURIComponent(query.scene)
     var that = this
-    console.log(scene);
-    this.setData({
-      scene: scene
+    if (scene) {
+      this.setData({
+        scene: scene
+      })
+    }
+    /* 获取用户信息 */
+    let tockiteData = {
+      userId: that.data.scene.slice(0, 21),
+      tockit: that.data.scene.slice(-10)
+    }
+    wx.request({
+      url: app.globalData.BASE_URL + '/wx/getUserInfo',
+      method: "POST",
+      data: tockiteData,
+      success (res) {
+        console.log(res);
+        if(res.data.code == 1) {
+          that.setData({
+            systemUserInfo: res.data.date
+          })
+          that.powerDrawer('open')
+        }else {
+          that.setData({
+            resultText: res.data.mess
+          })
+        }
+      }
     })
   },
   bindGetUserInfo(e) {
     let that = this
+    console.log(e);
     if (e.detail.userInfo) {
-      // 登录
-      wx.login({
-        success: res => {
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          var code = res.code; //返回code
-          app.globalData.js_code = res.code
-          wx.request({
-            url: app.globalData.BASE_URL + '/wx/authorizationLogin',
-            method: "POST",
-            data: {
-              js_code: app.globalData.js_code,
-              socketRadom: this.data.scene
-            },
-            success(res) {
-              if (res.data.code == 1) {
-                wx.showToast({
-                  title: '成功',
-                  icon: 'success',
-                  duration: 2000
-                })
-              }
-            }
+      wx.request({
+        url: app.globalData.BASE_URL + '/wx/getOpenid',
+        method: "POST",
+        data: {
+          js_code: app.globalData.js_code,
+          userId: that.data.scene.slice(0, 21),
+          userInfo: e.detail.userInfo
+        },
+        success (res) {
+          if(res.data.code == 1) {
+            wx.showToast({
+              title: '成功',
+              icon: 'success',
+              duration: 2000
+            })
+          }else {
+            wx.showToast({
+              title: res.data.mess,
+              icon: 'none',
+              duration: 2000
+            })
+          }
+          that.setData({
+            resultText: res.data.mess
           })
+          that.powerDrawer('close')
         }
       })
+    }
+  },
+  powerDrawer: function (flag) {
+    if(flag == 'close' || flag == 'open') {
+      this.util(flag)
+    }else {
+      wx.showToast({
+        title: '传递参数错误',
+        icon: 'none',
+        duration: 1500
+      })
+    }
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画
+    animation.opacity(0).rotateX(-100).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画
+      animation.left('50%')
+      animation.opacity(1).left('15%').rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
     }
   }
 })
