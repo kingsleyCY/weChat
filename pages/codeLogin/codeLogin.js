7bfc948b7d7f78097720547bc97ee4eeee56be51
cckings <1504468447@qq.com>
2019-01-01 18:33:28 +0800

    增加login判断

--- pages/codeLogin/codeLogin.js
+++ pages/codeLogin/codeLogin.js
@@ -20,79 +20,73 @@ Page({
         status: 'none'
       })
     }
+    if (!app.globalData.BASE_URL) {
+      wx.showLoading({
+        title: '请求中',
+      })
+      wx.request({
+        url: 'https://dev.lionynn.cn/apis/api/wx/getBaseURL',
+        method: "POST",
+        data: {},
+        success(res) {
+          wx.hideLoading()
+          app.globalData.BASE_URL = res.data.date.wxBaseUrl
+        },
+        error() {
+          wx.hideLoading()
+          app.globalData.BASE_URL = 'https://dev.lionynn.cn/apis/api'
+        }
+      })
+    }
   },
   bindGetUserInfo(e) {
     let that = this
     if (e.detail.userInfo) {
-      if (!app.globalData.BASE_URL) {
-        wx.showLoading({
-          title: '请求中',
-        })
-        wx.request({
-          url: 'https://dev.lionynn.cn/apis/api/wx/getBaseURL',
-          method: "POST",
-          data: {},
-          success(res) {
-            wx.hideLoading()
-            app.globalData.BASE_URL = res.data.date.wxBaseUrl
-            getUserInfo()
-          },
-          error() {
-            wx.hideLoading()
-            app.globalData.BASE_URL = 'https://dev.lionynn.cn/apis/api'
-          }
-        })
-      } else {
-        getUserInfo()
-      }
-
-      function getUserInfo() {
-        // 登录
-        wx.login({
-          success: res => {
-            var js_code = res.code;
-            wx.showLoading({
-              title: '请求中',
-            })
-            wx.request({
-              url: app.globalData.BASE_URL + '/wx/authorizationLogin',
-              method: "POST",
-              data: {
-                js_code: js_code,
-                socketRadom: this.data.scene
-              },
-              success(res) {
-                wx.hideLoading()
-                if (res.data.code == 1) {
-                  wx.showToast({
-                    title: '成功',
-                    icon: 'success',
-                    duration: 2000
-                  })
-                  that.setData({
-                    status: 'success'
-                  })
-                } else {
-                  wx.showToast({
-                    title: res.data.mess,
-                    icon: 'none',
-                    duration: 2000
-                  })
-                  that.setData({
-                    status: 'error'
-                  })
-                }
-              },
-              error() {
-                wx.hideLoading()
+      // 登录
+      wx.login({
+        success: res => {
+          var js_code = res.code;
+          wx.showLoading({
+            title: '请求中',
+          })
+          wx.request({
+            url: app.globalData.BASE_URL + '/wx/authorizationLogin',
+            method: "POST",
+            data: {
+              js_code: js_code,
+              socketRadom: this.data.scene
+            },
+            success(res) {
+              wx.hideLoading()
+              if (res.data.code == 1) {
+                wx.showToast({
+                  title: '成功',
+                  icon: 'success',
+                  duration: 2000
+                })
+                that.setData({
+                  status: 'success'
+                })
+              } else {
+                wx.showToast({
+                  title: res.data.mess,
+                  icon: 'none',
+                  duration: 2000
+                })
                 that.setData({
                   status: 'error'
                 })
               }
-            })
-          }
-        })
-      }
+            },
+            error() {
+              wx.hideLoading()
+              that.setData({
+                status: 'error'
+              })
+            }
+          })
+        }
+      })
     }
   }
 })
